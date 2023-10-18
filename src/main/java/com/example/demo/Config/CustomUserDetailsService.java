c914385359863dd6a488af12b20ab44aedb65786
Ian <zxc@zxc.com>
2023-10-18 22:51:46 +0600

    security add commit

---
+++ src/main/java/com/example/demo/Config/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.example.demo.Config;
+
+import com.example.demo.models.Client;
+import com.example.demo.repos.ClientRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private ClientRepo clientRepo;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Client client = clientRepo.findByLogin(username);
+        if (client == null) {
+            throw new UsernameNotFoundException("Пользователь не найден.");
+        }
+        if (!client.getPassword().equals(client.getPassword())) {
+            throw new UsernameNotFoundException("Неверный пароль.");
+        }
+
+        return org.springframework.security.core.userdetails.User.withUsername(client.getLogin())
+                .password(client.getPassword())
+                .roles(client.getRole())
+                .build();
+    }
+}
